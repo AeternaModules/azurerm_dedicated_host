ea5d5aff2bef89babc97cb05a0a1f5d43637fe51
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "dedicated_hosts" {
-  description = "All dedicated_host resources"
-  value       = azurerm_dedicated_host.dedicated_hosts
-}
 output "dedicated_hosts_auto_replace_on_failure" {
-  description = "List of auto_replace_on_failure values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.auto_replace_on_failure]
+  description = "Map of auto_replace_on_failure values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.auto_replace_on_failure }
 }
 output "dedicated_hosts_dedicated_host_group_id" {
-  description = "List of dedicated_host_group_id values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.dedicated_host_group_id]
+  description = "Map of dedicated_host_group_id values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.dedicated_host_group_id }
 }
 output "dedicated_hosts_license_type" {
-  description = "List of license_type values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.license_type]
+  description = "Map of license_type values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.license_type }
 }
 output "dedicated_hosts_location" {
-  description = "List of location values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.location]
+  description = "Map of location values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.location }
 }
 output "dedicated_hosts_name" {
-  description = "List of name values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.name]
+  description = "Map of name values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.name }
 }
 output "dedicated_hosts_platform_fault_domain" {
-  description = "List of platform_fault_domain values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.platform_fault_domain]
+  description = "Map of platform_fault_domain values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.platform_fault_domain }
 }
 output "dedicated_hosts_sku_name" {
-  description = "List of sku_name values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.sku_name]
+  description = "Map of sku_name values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.sku_name }
 }
 output "dedicated_hosts_tags" {
-  description = "List of tags values across all dedicated_hosts"
-  value       = [for k, v in azurerm_dedicated_host.dedicated_hosts : v.tags]
+  description = "Map of tags values across all dedicated_hosts, keyed the same as var.dedicated_hosts"
+  value       = { for k, v in azurerm_dedicated_host.dedicated_hosts : k => v.tags }
 }
 
